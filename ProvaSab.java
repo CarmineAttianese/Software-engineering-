2748a1df5739a2724bfe4c273c0103cf610e885a
sabricuo <s.cuomo22@studenti.unisa.it>
2023-11-16 11:57:17 +0100

    Inserimento file per testing "ProvaSab"

---
+++ ProvaSab.java
@@ -0,0 +1,14 @@
+public class ProvaSab {
+    public static void main(String[] args) {
+        // Stampiamo un messaggio di prova
+        System.out.println("Benvenuto nella classe ProvaSab!");
+
+        // Chiamiamo un metodo di prova
+        saluta();
+    }
+
+    // Metodo di prova
+    public static void saluta() {
+        System.out.println("Ciao da ProvaSab!");
+    }
+}
